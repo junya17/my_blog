d17087bfcd925ffbe24f16d1bb1677028d4c7faf
sumiya19 <tajnsy@gmail.com>
2018-12-05 23:54:22 +0900

    sign up delete

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get 'categories/show_admin/:id' => 'categories#show_admin'
   resources :categories
   devise_for :accounts
+  as :accounts do
+    get 'accounts/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'accounts' => 'devise/registrations#update', :as => 'user_registration'
+  end
   root to: 'posts#index'
   resources :admins
   resources :posts
